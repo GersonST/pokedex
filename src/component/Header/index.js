eb1aa6823db1996450d62b54643084482cd039ee
Gerson Severo <gersonsevero90@gmail.com>
2021-07-15 06:03:07 -0300

    Header
    Instalei o Axios e o Sass além de ter criado o componente Header e estilizado globalmente com a fonte Poppins

---
+++ src/component/Header/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.scss";
+
+export default function Header() {
+  return (
+    <div>
+      <header id="main-header">Pokedex</header>
+    </div>
+  );
+}
